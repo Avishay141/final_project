7825e1b9805305dd23e8a38cd3aa5519d3034ede
Avishay141 <Avishay141@gmail.com>
2020-08-03 15:06:23 +0300

    changes many files

--- index.js
+++ index.js
@@ -1,92 +1,55 @@
-var n = 0;
-var questions = [];
-questions.push("What is your name?");
-questions.push("How old are you?");
-questions.push("What do tou want from me?");
-questions.push("?שאלה בעברית");
 
-var num_of_questions = questions.length;
+// athounivation methods
+const  db = firebase.database().ref();
 
-var current_question = 0;
+$("#login_btn").on("click",function(){
+  var email = $("#email").val();
+  var password = $("#password").val();
+  var error_msg = document.getElementById("error_msg_id");
+  const auth = firebase.auth();
+  
+  var res = auth.signInWithEmailAndPassword(email, password);
+  res.catch(e => error_msg.innerHTML = e.message);
 
-$(".card .card-text").text(questions[0]);
-$("h1").css("color","white");
 
-
-$(".btn.btn-warning").on("click" , function(){
-  $(".card .card-text").text(questions[current_question % num_of_questions]);
-  $(".card .card-title").text("Question " + (current_question%num_of_questions+1) );
-  current_question++;
 });
+
+
+
+$("#sign_up_btn").on("click",function(){
+
+     window.location = "signup/signup.html";
+
+  });
+
+
+
+
+$("#forgot_btn").on("click",function(){
+  var auth = firebase.auth();
+  var email = $("#email").val();
+
+  auth.sendPasswordResetEmail(email).then(function() {
+    // Email sent.
+  }).catch(function(error) {
+    // An error happened.
+  });
+});
+
+
+
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    console.log("!!!!!!!!!: " + user.uid)
+    window.location = "questionnaire_page/questionnaire.html?uid="+user.uid;
+  } else {
+    console.log("User hasn't logged in yet");
+  }
+});
+
+
 
 // ---- Firebase function ------
 
 //###################################################
-
-
-
-//----On click function for Submit Button
-$(".submit_btn").on("click",function(){
-  var num = firebase.database().ref().child("num_of_questions");
-  var keys = Object.keys(num);
-  console.log("aaaaaaaaaaaaaaa");
-  console.log(keys);
-
-  window.alert("num of questions is: " + num[keys[0]]);
-
-  var db = firebase.database().ref();
-   db.child("questions/aaa").set({
-     question :{
-       q: "q1",
-       ans:{
-         ans1: "answer!!1",
-         ans2: "ans2!!!"
-       }
-     }
-   });
-  // db.child("questions").push().set({
-  //     question: [{
-  //       q: "how old are you?",
-  //       ans: "my answer zzzzzzzzzzzz"
-  //     }],
-  //     name: "zzzz"
-  // });
-//   firebase.database().ref('users/' + userId).set({
-//   username: name,
-//   email: email,
-//   profile_picture : imageUrl
-// });
-
-});
-
-
-
-//----On click function for Read Button
-$(".read_btn").on("click",function(){
-  console.log("trying to read data");
-  /*var db = firebase.database().ref();
-  var a = db.child("questions").orderByChild("name").equalTo("zz");
-  */
-  var ref = firebase.database().ref("questions/");
-  ref.once("value")
-    .then(function(snapshot) {
-      var v = snapshot.child("zz").val();
-      console.log("zz = " + v);
-
-      var v = snapshot.child("aaa/question/q").val();
-      console.log("q = " + v);
-
-      var v = snapshot.child("aaa/question/ans/ans1").val();
-      console.log("ans = " + v);
-
-
-
-      // var firstName = snapshot.child("name/first").val(); // "Ada"
-      // var lastName = snapshot.child("name").child("last").val(); // "Lovelace"
-      // var age = snapshot.child("age").val(); // null
-    });
-
-
-
-
-   });
