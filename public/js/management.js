8b25e9bcc1a5ddccdee2985c13a5d56efbfa51e8
nadavmg7 <Avishay141@gmail.com>
2020-10-26 19:32:58 +0200

    added admins list, add/remove options

--- public/js/management.js
+++ public/js/management.js
@@ -15,6 +15,16 @@ firebase.auth().onAuthStateChanged(function(user) {
 });
 
 
+
+async function create_admins_list(){
+  $('.list-group').empty();
+  var a =  await db.ref('Users').orderByChild('admin').equalTo(true).on("child_added" ,async function(snapshot) {
+    var value = await snapshot.val();
+    $('.list-group').append("<li class='list-group-item list-group-item-primary'> <b>" + "Name: "+'</b>'+value.name+ '<b>'+"  Email: " +'</b>' +value.userEmail +"</li>");
+  });
+
+}
+
 function get_userID_from_url() {
   var res = location.search.substring(1).split("=")[1];
   console.log("userID from url: " + res);
@@ -30,6 +40,75 @@ $("#move_to_main").on("click", function () {
   window.location = "../html_pages/questionnaire.html?uid="+userID;
 });
 
+$("#quest_btn").on("click", function () {
+  var admins_div = document.getElementById('admins_list');
+  admins_div.style.visibility = 'hidden';
+  var questions_div = document.getElementById('edit_quest_div');
+  questions_div.style.visibility = 'visible';
+});
+
+$("#add_admin_btn").on("click", async function () {
+  
+  var new_admin_email = document.getElementById("user_email_for_admin").value;
+  var uid_of_new_admin = await email_is_valid(new_admin_email);
+  var path = "Users/"+uid_of_new_admin;
+  if(uid_of_new_admin!=null){
+    await $('.list-group').empty();
+    await db.ref(path).update({
+      admin: true
+    });
+    await create_admins_list()
+  }
+  else
+  {
+    document.getElementById("error_message").innerHTML = "User dosen't exist";
+    $('#modalInputError').modal('show');
+  }
+ 
+});
+
+$("#remove_admin_btn").on("click", async function () {
+  
+  var admin_email = document.getElementById("user_email_for_admin").value;
+  var uid_of_admin = await email_is_valid(admin_email);
+  var path = "Users/"+uid_of_admin;
+  if(uid_of_admin!=null){
+    await $('.list-group').empty();
+    await db.ref(path).update({
+      admin: false
+    });
+    await create_admins_list()
+  }
+  else
+  {
+    document.getElementById("error_message").innerHTML = "User dosen't exist";
+    $('#modalInputError').modal('show');
+  }
+});
+
+
+
+
+async function email_is_valid(new_admin_email){
+  var path=null;
+  var a =  await db.ref('Users').orderByChild('userEmail').equalTo(new_admin_email).once('value').then(function(snapshot) {
+      var value = snapshot.val();
+      if(value!=null)
+      path=Object.keys(value);
+  });;
+ return path;
+}
+
+
+
+
+$("#manage_admins_btn").on("click", function () {
+  create_admins_list();
+  var questions_div = document.getElementById('edit_quest_div');
+  questions_div.style.visibility = 'hidden';
+  var admins_div = document.getElementById('admins_list');
+  admins_div.style.visibility = 'visible';
+});
 
 $("#upload_btn").on("click",async function(event){
   var input_file = document.getElementById("input_file");
